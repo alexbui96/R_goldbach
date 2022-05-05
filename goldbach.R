87438e7d071436a4996d482c35b7b0c02f48936c
alexbui96 <104482954+alexbui96@users.noreply.github.com>
2022-05-05 16:31:07 -0400

    Update goldbach.R

--- goldbach.R
+++ goldbach.R
@@ -1,23 +1,42 @@
 library(schoolmath)
 
-gb_list <- function(x) {
+gb_list <- function(n) {
   i <- 1
   #empty vector
   list <- NULL
   
-  while (i <= x/2) {
-    if ((i> 1) && is.prim(i) && is.prim(x-i)){
+  while (i <= n/2) {
+    if ((i> 1) && is.prim(i) && is.prim(n-i)){
       # add elements to list  
-      list <- append(list, sprintf("(%d, %d)  ", i, x-i))
+      list <- append(list, sprintf("(%d, %d)  ", i, n-i))
     }
-    i <- ifelse (x == 4, i+1, i+2)
+    i <- ifelse (n == 4, i+1, i+2)
   }
   return(list)
 }
 
-# input maximum number
-nmax <- 1000
-
-for (k in seq(4,nmax,2)) {
-  cat(sprintf("%d: ", k), gb_list(k), "\n")
+print_gb <- function(nmax) {
+  for (j in seq(4,nmax,2)) {
+    cat(sprintf("%d: ", j), gb_list(j), "\n")
+  }
 }
+
+gb_partition <- function(nmax) {
+  x <- NULL
+  y <- NULL
+  for (j in seq(4,nmax,2)) {
+    x <- append(x,j)
+    y <- append(y,length(gb_list(j)))
+  }
+  plot(x,y, xlab = "even number", ylab = "numbers of partitions")
+}
+
+complete <- function(nmax) {
+  print_gb(nmax)
+  gb_partition(nmax)
+}
+
+complete(1000)
+
+
+
